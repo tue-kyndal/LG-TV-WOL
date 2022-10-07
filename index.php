1a4170a29e1547a8ac00db27f56b3399d39b9616
tue-kyndal <106955975+tue-kyndal@users.noreply.github.com>
2022-10-07 12:07:54 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<head></head>
+<body>
+<center>
+<big>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+Wake DELLABLO from the web!
+<br>
+<br>
+Push this link ;O)
+<br>
+<br>
+<a href="wake-dellablo.php">Wake Dellablo</a><br>
+</big>
+</center>
+</body>
