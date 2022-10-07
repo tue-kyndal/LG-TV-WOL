77b89a53fa04f0c88ee25a00ecac4a6a30476eb6
tue-kyndal <106955975+tue-kyndal@users.noreply.github.com>
2022-10-07 12:09:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <center>
 <big>
 <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-Wake DELLABLO from the web!
+Wake Server from the LG TV Browser!
 <br>
 <br>
 Push this link ;O)
 <br>
 <br>
-<a href="wake-dellablo.php">Wake Dellablo</a><br>
+<a href="wake-dellablo.php">Wake Server</a><br>
 </big>
 </center>
 </body>
