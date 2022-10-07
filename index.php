95cf14059e5c5247d9504a1bff70c0b911d17661
tue-kyndal <106955975+tue-kyndal@users.noreply.github.com>
2022-10-07 12:16:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ Wake Server from the LG TV Browser!
 Push this link ;O)
 <br>
 <br>
-<a href="wake-dellablo.php">Wake Server</a><br>
+<a href="wake-server.php">Wake Server</a><br>
 </big>
 </center>
 </body>
